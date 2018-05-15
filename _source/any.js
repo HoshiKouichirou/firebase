4b461242ef0edfe64b79be043bf0cc645441dc55
System Administrator <root@hoshinoMacBook-2.local>
2018-05-15 10:27:02 +0900

    Initial Commit

---
+++ _source/any.js
@@ -0,0 +1,36 @@
+$(function() {
+  const Ref = firebase.database().ref('hiButton');
+
+	if(snapshot.val().hi_button !== true){
+  	let active = false; 
+		 }
+
+  function on(){
+    if(active === false){
+      active = true;
+      console.log("off")
+    }
+    else{
+      active = false;
+      console.log("on")
+    }
+    return active;
+  }
+
+  Ref.on('child_added', function(snapshot) {
+    if(snapshot.val().hi_button === true){
+      $("button").addClass("active");
+    }
+    if(snapshot.val().hi_button === false){
+      $("button").toggleClass("active");
+    }
+    active = snapshot.val().hi_button;
+  });
+
+  $("button").click(function(){
+    Ref.push({
+      hi_button: on()
+    });
+  });
+
+})
---
+++ _source/index.js
@@ -0,0 +1,12 @@
+  const config = {
+    apiKey: "AIzaSyBpwnGD9YV3ImBkm4dW-icXLUwhg1AieEM",
+    authDomain: "pet-meal.firebaseapp.com",
+    databaseURL: "https://pet-meal.firebaseio.com",
+    projectId: "pet-meal",
+    storageBucket: "pet-meal.appspot.com",
+    messagingSenderId: "41326479061"
+  };
+  firebase.initializeApp(config);
+$(function() {
+  console.log("hi");
+})
